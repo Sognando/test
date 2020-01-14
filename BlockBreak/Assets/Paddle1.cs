d0e3d070d3faf5a844b8dfe802315259fcd99faf
Sognando <hotub12@gmail.com>
2020-01-14 15:59:39 +0900

    update
    add project

---
+++ BlockBreak/Assets/Paddle1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = transform.position.x + Input.GetAxis("Horizontal");
+        transform.position = new Vector3(Mathf.Clamp(xPos, -5.5f, 5.5f), -9.5f, 0);
+    }
+}
